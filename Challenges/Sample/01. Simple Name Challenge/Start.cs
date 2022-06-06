ff785371fe99d0db41137a1a19038288023b7bf0
Nikola Luburic <luburic91@gmail.com>
2022-06-06 16:24:31 +0200

    feat: Adds two sample challenges.

---
+++ Challenges/Sample/01. Simple Name Challenge/Start.cs
@@ -0,0 +1,19 @@
+namespace Sample.Name
+{
+    /// <summary>
+    /// The ID of this challenge is displayed in the web UI.
+    /// 0. If you haven't already, install the Clean CaDET Plugin for Visual Studio Code.
+    /// 1. Hit Ctrl+Shift+P and run the "CCaDET Start" command.
+    /// 2. Enter your username and hit enter. If you do not get a successful login message visit the plugins wiki page.
+    /// 3. Right click the Start.cs file and run the "CCaDET Submit Challenge" command.
+    /// 4. Examine what the new panel is showing. Open and read the hint.
+    /// 5. Rename the "Completed" field according to the hint's instruction and save the change.
+    /// 6. Right click the Start.cs file and run the "CCaDET Submit Challenge" command.
+    /// 7. Analyze the panel with the new information and open the solution.
+    /// 8. On the web view hit the button to refresh the correctness level for the related challenge.
+    /// </summary>
+    public class Start
+    {
+        private string Completed;
+    }
+}
---
+++ Challenges/Sample/02. Simple Metric Challenge/Start.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Sample.Metrics
+{
+    /// <summary>
+    /// The ID of this challenge is displayed in the web UI.
+    /// 1. Read the comments in the method below and extract two methods from the main method.
+    /// 2. Right-click this file and run the "CCaDET Submit Challenge" command.
+    /// 3. If you did not complete the challenge read the hints and try again. Otherwise refresh the challenge correctness on the web UI.
+    /// </summary>
+    public class Start
+    {
+        public void Main()
+        {
+            // Extract the three lines of code below into a separate method.
+            Console.WriteLine("Print A1");
+            Console.WriteLine("Print A2");
+            Console.WriteLine("Print A3");
+
+            // Extract the three lines of code below into a separate method.
+            Console.WriteLine("Print B1");
+            Console.WriteLine("Print B2");
+            Console.WriteLine("Print B3");
+        }
+    }
+}
